18abe32e736ef0b2f2da1cb284a66c48f9cbf1d5
Sleeping-Donut <21991990+Sleeping-Donut@users.noreply.github.com>
2020-12-10 14:41:59 +0000

    added bashrc gitignore tmux config & vim syntax files. formatted setup chanaged defualt plugins vim

--- setup.sh
+++ setup.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+# A setup script to get any system prepped
+# S-D - 20/12/07
 
+######################
+# Platform Selector
+######################
+#PLATFORM=
+
+
+#######################
+# Mac
+#######################
+
+
+#######################
+# Applications
+#######################
+
+# install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
